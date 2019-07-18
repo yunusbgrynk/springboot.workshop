2bc7a2226f554c08fdb9c40088dc7b5d9dcf5e1a
Yunus Bağrıyanık <yunusbgrynk@gmail.com>
2019-07-18 23:06:34 +0300

    JpaRepository

--- src/main/java/com/yunusb/springboot/repository/EntityIIHistoryRepository.java
+++ src/main/java/com/yunusb/springboot/repository/EntityIIHistoryRepository.java
@@ -2,6 +2,6 @@ package com.yunusb.springboot.repository;
 
 import com.yunusb.springboot.model.EntityIIDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+//Spring Data
 public interface EntityIIHistoryRepository extends JpaRepository<EntityIIDetail, Long> {
 }
