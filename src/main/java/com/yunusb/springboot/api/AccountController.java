fc9d793e4ef20c1b47ffef66d688a7c71f378212
Yunus Bağrıyanık <yunusbgrynk@gmail.com>
2019-07-24 16:42:25 +0300

    create AccountController

--- src/main/java/com/yunusb/springboot/api/AccountController.java
+++ src/main/java/com/yunusb/springboot/api/AccountController.java
@@ -1,4 +1,13 @@
 package com.yunusb.springboot.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+
 public class AccountController {
+
+  @Autowired
+  private AuthenticationManager authenticationManager;
+
+  
+
 }
