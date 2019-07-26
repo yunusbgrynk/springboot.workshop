8a1e6f382e0d28f631fbad678afaa30034b7955f
Yunus Bağrıyanık <yunusbgrynk@gmail.com>
2019-07-26 11:24:34 +0300

    create front security settings

--- src/angular-frontend/src/app/security/jwt-interceptor.ts
+++ src/angular-frontend/src/app/security/jwt-interceptor.ts
@@ -5,6 +5,7 @@ import {Observable} from "rxjs";
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
   // enters between all requests, if localStorage have data(currentUser)
+  
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let currentUser = JSON.parse(localStorage.getItem('currentUser'));
     if( currentUser && currentUser.token ) {
