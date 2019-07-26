cbcc76602fe302477b8389d68ee0a75a91a2e0e8
Yunus Bağrıyanık <yunusbgrynk@gmail.com>
2019-07-26 11:22:55 +0300

    create front security settings

--- src/angular-frontend/src/app/app.module.ts
+++ src/angular-frontend/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {JwtInterceptor} from "./security/jwt-interceptor";
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
+
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/angular-frontend/src/app/security/jwt-interceptor.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@angular/core";
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+  // enters between all requests, if localStorage have data(currentUser)
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    if( currentUser && currentUser.token ) {
+      req = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${currentUser.token}`
+        }
+      });
+    }
+    return next.handle(req);
+  }
+
+}
